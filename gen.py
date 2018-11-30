b2a5fbb8bf8bd293f2d4383fcde12726e9ee782a
JupiterZhu <jupiter_zhu_007@126.com>
2018-11-29 21:37:25 -0500

    Mostly Completed

---
+++ gen.py
@@ -0,0 +1,26 @@
+import numpy as np
+import os
+import scipy
+from text_parsing import ret_parse
+import get_cmd
+
+test_dir = './'
+resources_dir = './resources/'
+dx_vst = resources_dir + 'mda_DX10.vst'
+midi_fl = resources_dir + 'midi_export.mid'
+generator = resources_dir + 'mrswatson64'
+# mr_w = resources_dir + 'mrswatson'
+
+fl_name = 'text.wav'
+
+ind_array = ret_parse()
+assert len(ind_array) == 157, (len(ind_array), ind_array)
+
+param_set = ""
+for x in range(1, len(ind_array) + 1):
+    param_set += "--parameter {},{} ".format(str(x), ind_array[x - 1])
+
+cmd = get_cmd.base_command(generator, dx_vst, midi_fl, param_set, fl_name)
+os.system.__call__(cmd)
+
+print("Done")
